2e30470108660e7443a44b025b09002a3069f005
shandilyaaryan <aryanshandilya10@gmail.com>
2024-10-21 20:16:01 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const process = require("process");
 const util = require("util");
 
@@ -22,6 +23,7 @@ function main() {
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   console.log("Logs from your program will appear here!");
+  console.log("Started")
 
   // Uncomment this block to pass the first stage
   if (command === "decode") {
